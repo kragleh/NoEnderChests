5f3d44712b28acd1a4bd484e0f101f3813105ac5
kragleh <danidurisik@gmail.com>
2023-06-09 16:55:07 +0200

    Initial Commit

--- src/main/java/com/kragleh/noenderchests/NoEnderChests.java
+++ src/main/java/com/kragleh/noenderchests/NoEnderChests.java
@@ -1,17 +1,33 @@
 package com.kragleh.noenderchests;
 
+import org.bukkit.ChatColor;
+import org.bukkit.block.EnderChest;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryOpenEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class NoEnderChests extends JavaPlugin {
+public final class NoEnderChests extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        saveDefaultConfig();
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    @EventHandler
+    public void onInventoryOpen(InventoryOpenEvent event) {
+        if (event.getPlayer().hasPermission(getConfig().getString("permission"))) return;
+
+        if (event.getInventory().getHolder() instanceof EnderChest) {
+            event.setCancelled(true);
+            event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("message")));
+        }
+    }
+
 }
