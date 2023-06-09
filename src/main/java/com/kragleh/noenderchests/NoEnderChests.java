598140482a8e82b00e606f486b5687f1aa6d807e
kragleh <danidurisik@gmail.com>
2023-06-09 16:54:54 +0200

    Initial commit

---
+++ src/main/java/com/kragleh/noenderchests/NoEnderChests.java
@@ -0,0 +1,17 @@
+package com.kragleh.noenderchests;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class NoEnderChests extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
